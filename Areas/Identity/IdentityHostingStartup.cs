d92bb4be239df647bf9aa8ea70c653a8dc3179cc
vuth108 <vuth108@gmail.com>
2022-01-19 16:55:49 +0700

    add identity scaffold

---
+++ Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using BlazorVNPTQuiz.Repository.DataContext;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(BlazorVNPTQuiz.Areas.Identity.IdentityHostingStartup))]
+namespace BlazorVNPTQuiz.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
