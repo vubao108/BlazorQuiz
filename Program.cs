dbadd9f744d901992bf3e7bf720b1a9cd94817a2
vuth108 <vuth108@gmail.com>
2022-01-22 16:45:23 +0700

    fix sync answer to db

--- Program.cs
+++ Program.cs
@@ -18,8 +18,14 @@ namespace BlazorVNPTQuiz
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+             //.ConfigureLogging(logging =>
+             //{
+             //    logging.ClearProviders();
+             //    logging.AddConsole();
+             //})
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
+                   
                     webBuilder.UseStartup<Startup>();
                 });
     }
