c37671ba8758142d542f95faa1c6adb778db5350
vuth108 <vuth108@gmail.com>
2022-01-24 01:04:14 +0700

    Use AppState to pass data

---
+++ Services/AppState.cs
@@ -0,0 +1,25 @@
+﻿using BlazorVNPTQuiz.Models;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorVNPTQuiz.Services
+{
+    public class AppState
+    {
+        public event Action<ComponentBase, string> StateChanged;
+
+        private void NotifyStateChanged(ComponentBase Source, string Property) => StateChanged?.Invoke(Source, Property);
+
+        public ExamInfo SelectedExamInfo {get;private set;}
+
+        public void UpdateSelectedExam(ComponentBase source, ExamInfo examInfo )
+        {
+            this.SelectedExamInfo = examInfo;
+            NotifyStateChanged(source, "SelectedExamInfo");
+        }
+
+    }
+}
