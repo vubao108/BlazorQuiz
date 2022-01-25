273fa02090ada1af2f6001ea336c8d8f7e30a3d8
vuth108 <vuth108@gmail.com>
2022-01-25 15:36:15 +0700

    add timer countdown in appstate

--- Services/AppState.cs
+++ Services/AppState.cs
@@ -4,16 +4,27 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Timers;
+using System.Diagnostics;
 
 namespace BlazorVNPTQuiz.Services
 ***REMOVED***
-    public class AppState
+    public class AppState : IDisposable
     ***REMOVED***
+        public AppState()
+        ***REMOVED***
+            StartTimer();
+           
+
+    ***REMOVED***
+        private Timer timer;
         public event Action<ComponentBase, string> StateChanged;
 
         private void NotifyStateChanged(ComponentBase Source, string Property) => StateChanged?.Invoke(Source, Property);
 
+        public UserInfo CurrentUset ***REMOVED*** get; private set; ***REMOVED***
         public ExamInfo SelectedExamInfo ***REMOVED***get;private set;***REMOVED***
+        public int RemainSeconds ***REMOVED*** get; private set; ***REMOVED*** = 0;
 
         public void UpdateSelectedExam(ComponentBase source, ExamInfo examInfo )
         ***REMOVED***
@@ -21,5 +32,45 @@ namespace BlazorVNPTQuiz.Services
             NotifyStateChanged(source, "SelectedExamInfo");
     ***REMOVED***
 
+        public void UpdateRemainSeconds(ComponentBase source, int RemainSeconds)
+        ***REMOVED***
+            this.RemainSeconds = RemainSeconds;
+           
+            NotifyStateChanged(source, "RemainSeconds");
+    ***REMOVED***
+
+       
+
+        public void StartTimer()
+        ***REMOVED***
+            Debug.Print("AppState.StartTimer()");
+            timer = new Timer(1000);
+            timer.Elapsed += OnTimedEvent;
+            timer.Enabled = true;
+    ***REMOVED***
+        void ReleaseTimer()
+        ***REMOVED***
+            
+            timer?.Stop();
+            
+            timer?.Dispose();
+    ***REMOVED***
+        private void OnTimedEvent(object sender, ElapsedEventArgs e)
+        ***REMOVED***
+            
+            if (--RemainSeconds >= 0)
+            ***REMOVED***
+                UpdateRemainSeconds(null, RemainSeconds);
+        ***REMOVED***
+            
+    ***REMOVED***
+
+        
+        public void Dispose()
+        ***REMOVED***
+            Debug.Print("AppState.Dispose()");
+            ReleaseTimer();
+            
+    ***REMOVED***
 ***REMOVED***
 ***REMOVED***
