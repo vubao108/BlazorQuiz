21ac7044591298f2578a4aa061dc21cc68e9f7d5
vuth108 <vuth108@gmail.com>
2022-01-25 20:50:03 +0700

    add timer with user id

--- Services/AppState.cs
+++ Services/AppState.cs
@@ -7,23 +7,42 @@ using System.Threading.Tasks;
 using System.Timers;
 using System.Diagnostics;
 using Microsoft.AspNetCore.Identity;
-
+using Microsoft.AspNetCore.Components.Authorization;
 namespace BlazorVNPTQuiz.Services
 {
     public class AppState : IDisposable
     {
-        public AppState()
+        private readonly AuthenticationStateProvider authenticationStateProvider;
+        private readonly UserManager<IdentityUser> userManager;
+        public AppState(AuthenticationStateProvider authenticationStateProvider, UserManager<IdentityUser> userManager)
         {
             StartTimer();
-           
 
+            this.authenticationStateProvider = authenticationStateProvider;
+            this.userManager = userManager;
+            CurrentIdentityUserId = GetUserId();
         }
+
+        private async Task<int> GetUserId()
+        {
+            var authState = await authenticationStateProvider.GetAuthenticationStateAsync();
+            if (authState.User.Identity.IsAuthenticated)
+            {
+                var user = userManager.Users.FirstOrDefault(item=>item.UserName == authState.User.Identity.Name);
+                
+                Debug.Print($"AppState.InitialUserId() : {user.Id}");
+                return Convert.ToInt32(user.Id);
+            }
+            return 0;
+        }
+
+
         private Timer timer;
         public event Action<ComponentBase, string> StateChanged;
 
         private void NotifyStateChanged(ComponentBase Source, string Property) => StateChanged?.Invoke(Source, Property);
 
-        public int  CurrentIdentityUserId { get; set; }
+        public Task<int>  CurrentIdentityUserId { get; set; }
         public ExamInfo SelectedExamInfo {get;private set;}
         public int RemainSeconds { get; private set; } = 0;
 
