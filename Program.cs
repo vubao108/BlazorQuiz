4924c3378318da41babd8c332dfc66b9e889cdbf
vuth108 <vuth108@gmail.com>
2022-01-27 15:34:41 +0700

    before remove log

--- Program.cs
+++ Program.cs
@@ -18,7 +18,7 @@ namespace BlazorVNPTQuiz
             var configuration = new ConfigurationBuilder()
            .SetBasePath(Directory.GetCurrentDirectory())
            .AddJsonFile("appsettings.json")
-           .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Production"}.json", true)
+        //   .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Production"}.json", true)
            .Build();
            
              Log.Logger = new LoggerConfiguration()
