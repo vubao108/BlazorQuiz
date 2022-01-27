1b37a8eec88a80f51c96a19a5adebd4fbd5e9e26
vuth108 <vuth108@gmail.com>
2022-01-27 17:25:12 +0700

    fix add seri log

--- Program.cs
+++ Program.cs
@@ -18,21 +18,22 @@ namespace BlazorVNPTQuiz
             var configuration = new ConfigurationBuilder()
            .SetBasePath(Directory.GetCurrentDirectory())
            .AddJsonFile("appsettings.json")
-        //   .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Production"}.json", true)
+              .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Production"}.json", true)
            .Build();
-           
-             Log.Logger = new LoggerConfiguration()
-            .ReadFrom.Configuration(configuration)
-            .CreateLogger();
+
+            Log.Logger = new LoggerConfiguration()
+           .ReadFrom.Configuration(configuration)
+           .CreateLogger();
             CreateHostBuilder(args).Build().Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args).UseSerilog()
-             
+
+
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                   
+
                     webBuilder.UseStartup<Startup>();
                 });
     }
