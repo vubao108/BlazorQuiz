932c691330ca372afc871a197dd7efd8cbf26051
vuth108 <vuth108@gmail.com>
2022-02-14 15:15:53 +0700

    add donvi vs chu de

---
+++ Models/Category.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorVNPTQuiz.Models
+***REMOVED***
+    public class Category
+    ***REMOVED***
+        public int Id ***REMOVED*** get; set; ***REMOVED***
+        public string Name ***REMOVED*** get; set; ***REMOVED***
+***REMOVED***
+***REMOVED***
