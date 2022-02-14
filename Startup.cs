b9c8373a52d5bc57462d96f229f3c380ba299c8c
vuth108 <vuth108@gmail.com>
2022-02-14 15:24:59 +0700

    add synfusion

--- Startup.cs
+++ Startup.cs
@@ -22,6 +22,7 @@ using BlazorVNPTQuiz.Services;
 using Blazored.Modal;
 using log4net;
 using Microsoft.Extensions.Logging;
+using Syncfusion.Blazor;
 
 namespace BlazorVNPTQuiz
 {
@@ -54,6 +55,8 @@ namespace BlazorVNPTQuiz
                     ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto;
             });
             services.AddServerSideBlazor();
+            services.AddSyncfusionBlazor(options => { options.IgnoreScriptIsolation = true; });
+
             services.AddScoped<AuthenticationStateProvider, RevalidatingIdentityAuthenticationStateProvider<IdentityUser>>();
            
            
