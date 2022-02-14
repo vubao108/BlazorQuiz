4796b3aca6b1385852f63c271aad31df035a8edf
vuth108 <vuth108@gmail.com>
2022-02-14 15:15:53 +0700

    add donvi vs chu de

---
+++ Models/Donvi.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorVNPTQuiz.Models
+{
+    public class Donvi
+    {
+        public int Id { get; set; }
+        public String Name { get; set; }
+
+
+    }
+
+    
+}
