346f72ee151ab6a1dfb8d9c6259cf0ec2265945e
vuth108 <vuth108@gmail.com>
2022-02-15 15:53:53 +0700

    Them repository on tap

---
+++ Repository/IRepositoryOntap.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BlazorVNPTQuiz.Models;
+
+namespace BlazorVNPTQuiz.Repository
+***REMOVED***
+    public interface IRepositoryOntap
+    ***REMOVED***
+        public List<Donvi> LayDanhSachDonVi(int user_id);
+
+        public List<Category> LayDanhSachChuDeTheoDonVi(int donvi_id);
+
+        public void GanChuDeChoDonVi(int donvi_id, List<Category> categories);
+
+        public void GenerateDulieuOnTap(int donvi_id);
+***REMOVED***
+***REMOVED***
