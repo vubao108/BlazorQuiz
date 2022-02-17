7deda1057250acabb61eafcaaa236ddcdae3ceac
vuth108 <vuth108@gmail.com>
2022-02-17 16:36:31 +0700

    Thêm trag ôn tập

---
+++ Models/QuestionLevel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorVNPTQuiz.Models
+{
+    public class QuestionLevel
+    {
+        public int Id;
+        public string Level;
+    }
+}
