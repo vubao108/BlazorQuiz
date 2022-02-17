4a3f5de38b2416a2ee4d02ae76e16f798de3e84c
vuth108 <vuth108@gmail.com>
2022-02-17 16:36:31 +0700

    Thêm trag ôn tập

--- Repository/MySqlRepositoryOntap.cs
+++ Repository/MySqlRepositoryOntap.cs
@@ -95,5 +95,33 @@ namespace BlazorVNPTQuiz.Repository
 
             return donvis;
         }
+
+        public async Task<List<Category>> LayDanhSachChuDeTheoNguoiDung(int user_id)
+        {
+            string sql_query = @"SELECT a.tag_id , a.tag_name, b.donvi_id,
+                0 isSelected
+                FROM tags a
+                , donvi_tag b, users c where a.tag_id = b.tag_id AND b.donvi_id = c.donvi_id and c.id = "
+                + $"{user_id}  order by  tag_name";
+            List<Category> categories = new List<Category>();
+
+            using (var connection = new MySqlConnection(configuration.GetConnectionString("DefaultConnection")))
+            {
+                using (var command = new MySqlCommand(sql_query, connection))
+                {
+                    await connection.OpenAsync();
+                    using var reader = command.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        int tag_id = reader.GetInt32("tag_id");
+                        String tag_name = reader.GetString("tag_name");
+                        bool IsSelected = reader.GetInt32("isSelected") == 1;
+                        categories.Add(new Category() { Id = tag_id, Name = tag_name, IsSelected = IsSelected });
+                    }
+                }
+            }
+
+            return categories;
+        }
     }
 }
