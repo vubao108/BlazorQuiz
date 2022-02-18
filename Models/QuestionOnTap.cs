5fa1291a2b8d36edd55e755d1b0146634cdc7ba3
vuth108 <vuth108@gmail.com>
2022-02-18 08:15:41 +0700

    Lay cau hoi on tap theo muc do

---
+++ Models/QuestionOnTap.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace BlazorVNPTQuiz.Models
+***REMOVED***
+    public class QuestionOnTap
+    ***REMOVED***
+        public int Id;
+        public int UserId ***REMOVED*** get; set; ***REMOVED***
+        public int QuestionId ***REMOVED*** get; set; ***REMOVED***
+        public string QuestionText ***REMOVED*** get; set; ***REMOVED***
+
+        public int LevelId ***REMOVED*** get; set; ***REMOVED***
+        public int TagId ***REMOVED*** get; set; ***REMOVED***
+        public bool IsMultipleChoice ***REMOVED*** get; set; ***REMOVED***
+        public List<AnswerDAO> Answers ***REMOVED***get;set;***REMOVED***
+***REMOVED***
+***REMOVED***
--- Repository/MySqlRepositoryOntap.cs
+++ Repository/MySqlRepositoryOntap.cs
@@ -148,5 +148,55 @@ namespace BlazorVNPTQuiz.Repository
 
             return levels;
     ***REMOVED***
+
+        public async Task<List<QuestionOnTap>> LayDanhSachCauHoiOnTapTheoMucDo(int user_id, List<int> tag_ids, int level_id)
+        ***REMOVED***
+            string tag_ids_str = String.Join(',', tag_ids.ToArray());
+            string sql_query = $" call  proc_lay_cauhoi_ontap_web(***REMOVED***user_id***REMOVED***,'***REMOVED***tag_ids_str***REMOVED***', ***REMOVED***level_id***REMOVED***)";
+
+            List<QuestionOnTap> questionOnTaps = new List<QuestionOnTap>();
+
+            using (var connection = new MySqlConnection(configuration.GetConnectionString("DefaultConnection")))
+            ***REMOVED***
+                using (var command = new MySqlCommand(sql_query, connection))
+                ***REMOVED***
+                    await connection.OpenAsync();
+                    using var reader = command.ExecuteReader();
+                    while (reader.Read())
+                    ***REMOVED***
+                        AnswerDAO answerDAO = new AnswerDAO()
+                        ***REMOVED***
+                            AnswerId = reader.GetInt32("answer_id"),
+                            AnswerText = reader.GetString("answer_text"),
+                            IsCorrect = reader.GetInt32("state") == 1
+                    ***REMOVED***;
+                        QuestionOnTap questionOnTap = new QuestionOnTap()
+                        ***REMOVED***
+                            Id = reader.GetInt32("id"),
+                            QuestionId = reader.GetInt32("question_id"),
+                            QuestionText = reader.GetString("question_text"),
+                            IsMultipleChoice = reader.GetInt32("is_multiple_choice") == 1,
+                            TagId = reader.GetInt32("tag_id"),
+                            LevelId = reader.GetInt32("level_id"),
+                            UserId = user_id
+                    ***REMOVED***;
+                        QuestionOnTap currentQuestionOntap = questionOnTaps.FirstOrDefault(item => item.QuestionId == questionOnTap.QuestionId);
+                        if(currentQuestionOntap == null)
+                        ***REMOVED***
+                            questionOnTap.Answers = new List<AnswerDAO> ***REMOVED*** answerDAO ***REMOVED***;
+                            questionOnTaps.Add(questionOnTap);
+                    ***REMOVED***
+                        else
+                        ***REMOVED***
+                            questionOnTap.Answers.Add(answerDAO);
+                    ***REMOVED***
+                        
+                ***REMOVED***
+            ***REMOVED***
+        ***REMOVED***
+
+            return questionOnTaps;
+    ***REMOVED***
+
 ***REMOVED***
 ***REMOVED***
